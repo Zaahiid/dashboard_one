f1a1b5f006cc4f0a8761b2fe8b21f63e56d6adb2
Zaahiid <zahidrsheikh01@gmail.com>
2023-11-04 22:47:16 +0530

    changes for deployment

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/dashboard_one/', // Set the base path for deployment
+  build: {
+    outDir: 'dist', // Output directory for build
+  },
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
+});
